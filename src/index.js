d195c17d096424a01fde490356898d7fbda86ebf
den <you@example.com>
2025-08-28 14:41:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,30 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  const MorseTableWithSpace = { ...MORSE_TABLE, '**********': ' ' };
+  const pieceSize = 10;
+  const pieces = Array.from(
+    { length: Math.ceil(expr.length / pieceSize) },
+    (_, i) => expr.slice(i * pieceSize, i * pieceSize + pieceSize)
+  );
+  const decodedPieces = pieces.map((piece) => {
+    if (piece === '**********') {
+      return ' ';
+    }
+    const pairSize = 2;
+    const pairs = Array.from({ length: piece.length / pairSize }, (_, i) =>
+      piece.slice(i * pairSize, i * pairSize + pairSize)
+    );
+    const morseSymbol = pairs
+      .map((pair) => {
+        if (pair === '10') return '.';
+        if (pair === '11') return '-';
+        return '';
+      })
+      .join('');
+    return MorseTableWithSpace[morseSymbol];
+  });
+  return decodedPieces.join('');
 };
